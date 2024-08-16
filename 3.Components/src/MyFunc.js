e367fdfc9af93020189881d0ff4c4ea064f90ddb
nikhilm28 <nikhil.madne.8@gmail.com>
2024-08-16 10:17:03 +0530

    Nested Components Example

--- 3.Components/src/MyFunc.js
+++ 3.Components/src/MyFunc.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
-const MyFunc1 = () => {
-  return <h3>External Function1</h3>
-}
+// const MyFunc1 = () => {
+//   return <h3>External Function1</h3>
+// }
 
-const MyFunc2 = () => {
-  return <h3>External Function2</h3>
+// const MyFunc2 = () => {
+//   return <h3>External Function2</h3>
+// }
+
+const MyFunc1 = () => {
+  const MyFunc2 = () => {
+    return <h3>External Function2</h3>
+  }
+  return (
+    <div>
+      <h3>External Function1</h3>
+      <MyFunc2 />
+    </div>
+  )
 }
 
 // export default MyFunc
-export { MyFunc1 } //if need to export many different functions use this
-export default MyFunc2 //if onlu 1 function use these
+// export { MyFunc1 } //if need to export many different functions use this
+export default MyFunc1 //if onlu 1 function use these
