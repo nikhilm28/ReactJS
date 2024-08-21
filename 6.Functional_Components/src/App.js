c3745c57a024155eb2fb6529bcc4308c0c420ec5
nikhilm28 <nikhil.madne.8@gmail.com>
2024-08-21 10:01:24 +0530

    Functional Components Example

---
+++ 6.Functional_Components/src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+import react, { useState } from 'react'
+
+function App() {
+  // let color = 'red'
+  const [color, setColor] = useState('red')
+  const changeColor = () => {
+    setColor('green')
+    console.log('Color: ', color)
+  }
+  return (
+    <div>
+      <h1>Hello ReactJS!!</h1>
+      <h2>Color Value {color}</h2>
+      <button onClick={changeColor}>Change Color</button>
+    </div>
+  )
+}
+
+export default App
